ccf64ebdca201017b6e7ea4002c5f101759f6409
Yusuf Ali <yusufofauburn@gmail.com>
2026-09-19 20:40:39 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use macroquad::prelude::*;
+
+#[macroquad::main("MyGame")]
+async fn main() {
+    loop {
+        clear_background(RED);
+
+        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
+        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
+
+        draw_text("Hello, Macroquad!", 20.0, 20.0, 30.0, DARKGRAY);
+
+        next_frame().await
+    }
+}
